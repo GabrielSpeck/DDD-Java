d5a8ced40c54aa042b7226beed61b7c3fdae88a6
Joao toledo <jvmstoledo@gmail.com>
2026-04-10 21:17:29 -0300

    add

---
+++ CP1/src/java/com/locadora/cliente/CNH.java
@@ -0,0 +1,17 @@
+package com.locadora.cliente;
+import java.time.LocalDate;
+
+public class CNH {
+
+    private final String numero;
+    private final LocalDate dataVencimento;
+
+    public CNH(String numero, LocalDate dataVencimento) {
+        this.numero = numero;
+        this.dataVencimento = dataVencimento;
+    }
+
+    public boolean estaValida() {return !dataVencimento.isBefore(LocalDate.now());}
+    public String getNumero() {return numero;}
+    public LocalDate getDataVencimento() {return dataVencimento;}
+}
